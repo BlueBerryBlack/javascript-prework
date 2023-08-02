ec7c5428b36cd092ce0aca923c6f6ad0b2e4cece
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2023-08-02 13:23:08 +0200

    Basic code js playerMove

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ computerMove = 'papier' ;
 } else if (randomNumber == '3') {
     computerMove = 'nożyce' ;
 }
+printMessage('Mój ruch:' + computerMove);
+
+var playerMove, playerInput;
+playerInput - prompt('Wybierz swój ruch! 1:Kamień 2:Papier 3:Nożyce');
+console.log('Wpisana odpowiedź to:' +playerInput);
+if (playerInput == '1'){
+    playerMove='Kamień';
+} else if (playerInput == '2'){
+    playerMove='Papier';
+} else if (playerInput == '3'){
+    playerMove='Nożyce';
+} else (){
+    playerMove='Nieznany ruch';
+}
+printMessage('Twój ruch:' + playerMove);
